a120fba8f6b4c8a5fc5185912a2d014f566999a4
DannyS712 <DannyS712.enwiki@gmail.com>
2020-06-11 20:36:21 +0000

    Add sidebar link to GlobalWatchlist from Special:Watchlist
    Add handling for the `SkinBuildSidebar` hook
    
    Uses the new Hook registration system, with a hook handler and DI
    rather than static methods and lazy initialization.
    
    Hook handler has 100% test coverage with pure Unit tests.
    
    Bug: T255205
    Change-Id: I41a8f80d721d8ee0e2de716c003b2ed0d0c82e6d

---
+++ includes/GlobalWatchlistHooks.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Hook handler
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup SpecialPage
+ */
+
+namespace MediaWiki\Extension\GlobalWatchlist;
+
+use MediaWiki\Hook\SkinBuildSidebarHook;
+use MediaWiki\SpecialPage\SpecialPageFactory;
+use Skin;
+
+/**
+ * @author DannyS712
+ */
+class GlobalWatchlistHooks implements SkinBuildSidebarHook {
+
+	/** @var SpecialPageFactory */
+	private $specialPageFactory;
+
+	/**
+	 * @param SpecialPageFactory $specialPageFactory
+	 */
+	public function __construct( SpecialPageFactory $specialPageFactory ) {
+		$this->specialPageFactory = $specialPageFactory;
+	}
+
+	/**
+	 * @param Skin $skin
+	 * @param array &$bar Sidebar contents. Modify $bar to add or modify sidebar portlets.
+	 * @return bool|void True or no return value to continue or false to abort
+	 */
+	public function onSkinBuildSidebar( $skin, &$bar ) {
+		$title = $skin->getTitle();
+		$onWatchlist = $title->isSpecial( 'Watchlist' );
+		if ( !$onWatchlist ) {
+			return;
+		}
+
+		$globalWatchlistTitle = $this->specialPageFactory
+			->getPage( 'GlobalWatchlist' )
+			->getPageTitle();
+
+		$link = [
+			'text' => $skin->msg( 'globalwatchlist-gotoglobal' )->text(),
+			'href' => $globalWatchlistTitle->getLinkURL(),
+			'title' => $skin->msg( 'globalwatchlist-gotoglobal-tooltip' )->text(),
+		];
+		$bar['navigation'][] = $link;
+	}
+
+}
---
+++ tests/phpunit/unit/GlobalWatchlistHooksTest.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace MediaWiki\Extension\GlobalWatchlist;
+
+use MediaWiki\SpecialPage\SpecialPageFactory;
+use MediaWikiUnitTestCase;
+use Message;
+use Skin;
+use SpecialPage;
+use Title;
+
+/**
+ * Tests for the hook handler
+ *
+ * @author DannyS712
+ * @covers MediaWiki\Extension\GlobalWatchlist\GlobalWatchlistHooks
+ */
+class GlobalWatchlistHooksTest extends MediaWikiUnitTestCase {
+
+	public function testLinkNotAdded() {
+		$specialPageFactory = $this->createNoOpMock( SpecialPageFactory::class );
+		$hookHandler = new GlobalWatchlistHooks( $specialPageFactory );
+
+		$title = $this->createMock( Title::class );
+		$title->expects( $this->once() )
+			->method( 'isSpecial' )
+			->with( $this->equalTo( 'Watchlist' ) )
+			->willReturn( false );
+
+		$skin = $this->createMock( Skin::class );
+		$skin->expects( $this->once() )
+			->method( 'getTitle' )
+			->willReturn( $title );
+
+		$sidebar = [];
+		$hookHandler->onSkinBuildSidebar( $skin, $sidebar );
+		$this->assertArrayEquals(
+			[],
+			$sidebar,
+			'If not on Special:Watchlist, the sidebar should not change'
+		);
+	}
+
+	public function testLinkAdded() {
+		$globalWatchlistTitle = $this->createMock( Title::class );
+		$globalWatchlistTitle->expects( $this->once() )
+			->method( 'getLinkURL' )
+			->willReturn( 'Special:GlobalWatchlist href goes here' );
+
+		$specialGlobalWatchlist = $this->createMock( SpecialPage::class );
+		$specialGlobalWatchlist->expects( $this->once() )
+			->method( 'getPageTitle' )
+			->willReturn( $globalWatchlistTitle );
+
+		$specialPageFactory = $this->createMock( SpecialPageFactory::class );
+		$specialPageFactory->expects( $this->once() )
+			->method( 'getPage' )
+			->with( $this->equalTo( 'GlobalWatchlist' ) )
+			->willReturn( $specialGlobalWatchlist );
+
+		$hookHandler = new GlobalWatchlistHooks( $specialPageFactory );
+
+		$title = $this->createMock( Title::class );
+		$title->expects( $this->once() )
+			->method( 'isSpecial' )
+			->with( $this->equalTo( 'Watchlist' ) )
+			->willReturn( true );
+
+		$textMessage = $this->createMock( Message::class );
+		$textMessage->expects( $this->once() )
+			->method( 'text' )
+			->willReturn( 'Link text goes here' );
+		$titleMessage = $this->createMock( Message::class );
+		$titleMessage->expects( $this->once() )
+			->method( 'text' )
+			->willReturn( 'Tooltip text goes here' );
+
+		$skin = $this->createMock( Skin::class );
+		$skin->expects( $this->once() )
+			->method( 'getTitle' )
+			->willReturn( $title );
+		$skin->expects( $this->exactly( 2 ) )
+			->method( 'msg' )
+			->withConsecutive(
+				[ $this->equalTo( 'globalwatchlist-gotoglobal' ) ],
+				[ $this->equalTo( 'globalwatchlist-gotoglobal-tooltip' ) ]
+			)
+			->will(
+				$this->onConsecutiveCalls(
+					$textMessage,
+					$titleMessage
+				)
+			);
+
+		$sidebar = [];
+		$sidebar['navigation'] = [];
+
+		$hookHandler->onSkinBuildSidebar( $skin, $sidebar );
+
+		$expectedSidebar = [
+			'navigation' => [
+				[
+					'text' => 'Link text goes here',
+					'href' => 'Special:GlobalWatchlist href goes here',
+					'title' => 'Tooltip text goes here'
+				]
+			]
+		];
+
+		$this->assertArrayEquals(
+			$expectedSidebar,
+			$sidebar,
+			'If on Special:Watchlist, the sidebar should have a link added'
+		);
+	}
+
+}
