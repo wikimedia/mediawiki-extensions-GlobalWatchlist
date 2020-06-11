b56ed449cf683078dfe87d3c32ace66f02617ff9
DannyS712 <DannyS712.enwiki@gmail.com>
2020-06-11 15:06:45 +0000

    GlobalWatchlist extension v0.1 - just a shell
    Adds the special pages to load the javascript for the front-end,
    but no javascript yet (for now just loads a placeholder script
    that logs to the console to confirm that it was loaded)
    
    Existing messages were added in i18n/script/en.json so that translation
    can begin on those once this is merged, without needing to wait until
    the script begins to use them.
    
    This is nowhere near complete, but its a start, and a good break point
    to separate the javascript migration from the basic framework.
    
    Bug: T255172
    Change-Id: I2ac5314ebcb898593497082c5b7b4b1335d89706

---
+++ includes/SpecialGlobalWatchlistSettings.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Implements Special:GlobalWatchlistSettings
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup SpecialPage
+ */
+
+namespace MediaWiki\Extension\GlobalWatchlist;
+
+use Html;
+use SpecialPage;
+
+/**
+ * Doesn't do anything other than show a message for now
+ *
+ * @ingroup SpecialPage
+ * @author DannyS712
+ */
+class SpecialGlobalWatchlistSettings extends SpecialPage {
+
+	public function __construct() {
+		parent::__construct( 'GlobalWatchlistSettings' );
+	}
+
+	/**
+	 * @param string|null $par
+	 */
+	public function execute( $par ) {
+		$this->setHeaders();
+
+		$this->addHelpLink( 'Extension:GlobalWatchlist' );
+
+		$out = $this->getOutput();
+		$out->addModules( 'ext.globalwatchlist.specialglobalwatchlistsettings' );
+
+		// Class is for JS to detect and replace
+		$message = Html::rawElement(
+			'div',
+			[ 'class' => 'globalwatchlist-content' ],
+			$this->msg( 'globalwatchlist-javascript-required' )
+		);
+		$out->addHTML( $message );
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function getGroupName() {
+		return 'changes';
+	}
+
+}
---
+++ modules/ext.globalwatchlist.specialglobalwatchlist.js
@@ -0,0 +1,12 @@
+/**
+ * Javascript for Special:GlobalWatchlist
+ */
+( function () {
+	'use strict';
+
+	// On ready initialization
+	$( function () {
+		/* eslint-disable-next-line no-console */
+		console.log( 'GlobalWatchlist javascript loaded!' );
+	} );
+}() );
