883a7810ce91250aebc7253381d0845923f37b68
DannyS712 <DannyS712.enwiki@gmail.com>
2020-06-14 05:08:54 +0000

    Fix a typo: settinsg -> settings
    Change-Id: Idf79c1eb8a958cdb5f3aedd121247d63b3df1bd2

--- includes/SettingsManager.php
+++ includes/SettingsManager.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Backend for setting (and eventually retrieving) user settinsg
+ * Backend for setting (and eventually retrieving) user settings
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
