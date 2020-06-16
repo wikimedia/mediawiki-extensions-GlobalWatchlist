3f0c32b23187ff581811719669a0070d78c9d24a
DannyS712 <DannyS712.enwiki@gmail.com>
2020-06-16 04:55:08 +0000

    Settings: Lazy-load api module
    Not needed until the user tries to save, don't delay startup
    
    Change-Id: Icf233d7c598b9fa7295b4f0aaf3c3abbd58a3d2e

--- modules/ext.globalwatchlist.saveSettings.js
+++ modules/ext.globalwatchlist.saveSettings.js
@@ -31,7 +31,11 @@ function GlobalWatchlistSaveSettings( GlobalWatchlistDebug, newSettings ) {
 
 	GlobalWatchlistDebug.info( 'SaveSettings - parameters', params, 1 );
 
-	return new mw.Api().postWithEditToken( params );
+	// Not loaded as a dependency in extension.json because it isn't needed until the user
+	// wants to save settings, and shouldn't delay the initial startup
+	return mw.loader.using( 'mediawiki.api' ).then( function () {
+		return new mw.Api().postWithEditToken( params );
+	} );
 }
 
 module.exports = GlobalWatchlistSaveSettings;
