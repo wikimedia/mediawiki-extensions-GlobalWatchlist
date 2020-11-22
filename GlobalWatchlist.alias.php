32fa7c49e403d015fe49f71706cd7bf05f0d1901
Amir Aharoni <amir.aharoni@mail.huji.ac.il>
2020-11-22 13:44:49 +0200

    Add Hebrew special page alias
    Change-Id: I232307d3c4b80c748e7860e09d66771e4b3504a7

--- GlobalWatchlist.alias.php
+++ GlobalWatchlist.alias.php
@@ -19,3 +19,9 @@ $specialPageAliases['de'] = [
 	'GlobalWatchlistSettings' =>
 		[ 'Globale_Beobachtungsliste_Einstellungen', 'Einstellungen_Globale_Beobachtungsliste' ],
 ];
+
+/** Hebrew (עברית) */
+$specialPageAliases['he'] = [
+	'GlobalWatchlist' => [ 'רשימת_מעקב_גלובלית' ],
+	'GlobalWatchlistSettings' => [ 'הגדרות_רשימת_מעקב_גלובלית' ],
+];
