8ffefbad268f515114f25fe1f0dc3762fd95e134
DannyS712 <dannys712.wiki@gmail.com>
2021-03-16 20:10:58 +0000

    Add @codeCoverageIgnore for ServiceWiring
    Tested in ServiceWiringTest, but phpunit doesn't
    understand tests for code outside of classes/functions
    
    Change-Id: Ie6a74d3d17af0f04f4f29903a38fa5a26c8bfa15

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -4,6 +4,13 @@ use MediaWiki\Extension\GlobalWatchlist\SettingsManager;
 use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\MediaWikiServices;
 
+/**
+ * PHPUnit doesn't understand code coverage for code outside of classes/functions,
+ * like service wiring files. This *is* tested though, see
+ * tests/phpunit/integration/ServiceWiringTest.php
+ *
+ * @codeCoverageIgnore
+ */
 return [
 	'GlobalWatchlistSettingsManager' => function (
 		MediaWikiServices $services
