2fe8dafab4318881cfffb2efcfea514483900647
DannyS712 <dannys712.wiki@gmail.com>
2021-04-20 07:54:35 +0000

    Use codeCoverageIgnore(Start|End) for ServiceWiring.php
    Per doc.wikimedia.org results, the @codeCoverageIgnore
    isn't doing it, perhaps because its not a class or method
    
    Change-Id: I01e7dbd6c9af068e5f97cdd10500fe4f324a38a5

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -4,13 +4,11 @@ use MediaWiki\Extension\GlobalWatchlist\SettingsManager;
 use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\MediaWikiServices;
 
-/**
- * PHPUnit doesn't understand code coverage for code outside of classes/functions,
- * like service wiring files. This *is* tested though, see
- * tests/phpunit/integration/ServiceWiringTest.php
- *
- * @codeCoverageIgnore
- */
+// PHPUnit doesn't understand code coverage for code outside of classes/functions,
+// like service wiring files. This *is* tested though, see
+// tests/phpunit/integration/ServiceWiringTest.php
+// @codeCoverageIgnoreStart
+
 return [
 	'GlobalWatchlistSettingsManager' => static function (
 		MediaWikiServices $services
@@ -22,3 +20,5 @@ return [
 		);
 	},
 ];
+
+// @codeCoverageIgnoreEnd
