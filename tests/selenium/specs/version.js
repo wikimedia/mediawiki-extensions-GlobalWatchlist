39af7c6b9ad52b01ffca0cbfc15b89f921d93650
DannyS712 <dannys712.wiki@gmail.com>
2021-06-09 06:19:37 +0000

    Set up selenium for testing, add a first basic test
    First test just ensures that the extension is showing
    up as listed on Special:Version, as a demonstration
    that selenium is set up and working - this appears
    to be the standard demo test for new extensions.
    
    This test will be deleted in the next commit.
    
    Bug: T284521
    Change-Id: Id2bcf08d1b940649dbabc22f39100bb9e5a9f665

---
+++ tests/selenium/specs/version.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var assert = require( 'assert' ),
+	// this is just a sample on how to use a page
+	VersionPage = require( '../pageobjects/version.page' );
+
+describe( 'GlobalWatchlist', function () {
+
+	// this is just a sample test
+	it( 'is configured correctly', function () {
+		VersionPage.open();
+
+		// this is just a sample assertion, checking if an element exists
+		assert( VersionPage.extension.isExisting() );
+
+	} );
+
+} );
