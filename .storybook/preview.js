54409fc629179e48ae45f893e290fa0c01972a8d
Ed Sanders <esanders@wikimedia.org>
2025-02-27 12:35:33 +0000

    Follow-up Ib0bff44fe: Remove .storybook files & ESLint vue config
    WVUI was dropped in T310240 so this is no longer used.
    
    Change-Id: I2e594a5e1fb964e6f4d4c7e279cb267ea1ddf1a6

--- .storybook/preview.js
+++
@@ -1,7 +0,0 @@
-import './../node_modules/@wikimedia/wvui/dist/commonjs2/wvui.css';
-
-import Vue from 'vue';
-import VueI18n from './VueI18n.js';
-
-// Fake version of core's $i18n plugin
-Vue.use( VueI18n );
